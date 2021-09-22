84fb00573429b12ac7a1a1a58cfe0b31bfe996fd
Anurag Sinha <sinhaanurag18@gmail.com>
2021-09-22 13:49:23 +0530

    Stringproject

---
+++ src/com/company/StringBasic.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class StringBasic {
+    public static void main(String[] args) {
+        String name="Rishabh";
+        System.out.println(name.charAt(4));
+
+    }
+}
