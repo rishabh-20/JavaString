8bf1d153eef8833c0177b36953c9c84d3f548b4a
Anurag Sinha <sinhaanurag18@gmail.com>
2021-09-22 13:33:06 +0530

    Stringproject

--- src/com/company/StringA.java
+++ src/com/company/StringA.java
@@ -4,7 +4,9 @@ public class StringA {
     public static void main(String[] args) {
 
         String name="Rishabh";
+        String name2="Narayan";
         System.out.println(name);
+        System.out.println(name2);
 
     }
 }
